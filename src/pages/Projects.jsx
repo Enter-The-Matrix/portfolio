e2e4e9c058f840e25f46ae4f507371197a21b66d
Ashwani Sajwan <82757602+Enter-The-Matrix@users.noreply.github.com>
2024-05-07 19:16:45 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,7 +14,7 @@ function Projects() {
   const projects = [
     {
       name: "Chatty app ",
-      description: "Real Time Chat app (may take time to load as hosted on free tier)",
+      description: "Real Time Chat app (may take time to load as project is hosted on free tier)",
       link: "https://chatty-react-app.onrender.com/",
       img: [chatty1, chatty2, chatty3, chatty4],
     },
